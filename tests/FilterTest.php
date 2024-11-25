0dd8049221f6095747e311a88233c2d05be3c711
aporat28 <aporat28@gmail.com>
2024-11-24 20:21:26 -0500

    tests

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -7,15 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class FilterTest extends TestCase
 {
-    public function testTrimFilter(): void
-    {
-        $value = ' ValUe INPUT';
 
-        $filter = new FilterVar();
-        $result = $filter->filterValue('trim', $value);
-
-        $this->assertEquals('ValUe INPUT', $result);
-    }
 
     public function testCapitalizeFilter(): void
     {
@@ -36,4 +28,45 @@ class FilterTest extends TestCase
 
         $this->assertIsInt($result);
     }
+
+    public function testLowercaseFilter(): void
+    {
+        $value = 'ValUe INPUT';
+
+        $filter = new FilterVar();
+        $result = $filter->filterValue('lowercase', $value);
+
+        $this->assertEquals('value input', $result);
+    }
+
+    public function testStripTagsFilter(): void
+    {
+        $value = '<html>ValUe INPUT</html';
+
+        $filter = new FilterVar();
+        $result = $filter->filterValue('strip-tags', $value);
+
+        $this->assertEquals('ValUe INPUT', $result);
+    }
+
+    public function testTrimFilter(): void
+    {
+        $value = ' ValUe INPUT';
+
+        $filter = new FilterVar();
+        $result = $filter->filterValue('trim', $value);
+
+        $this->assertEquals('ValUe INPUT', $result);
+    }
+
+    public function testUppercaseFilter(): void
+    {
+        $value = 'ValUe INPUT';
+
+        $filter = new FilterVar();
+        $result = $filter->filterValue('uppercase', $value);
+
+        $this->assertEquals('VALUE INPUT', $result);
+    }
+
 }
